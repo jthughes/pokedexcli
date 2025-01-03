5b049f33ca189312029b55c31c2fb3c51e20e898
Jeremy Hughes <jthughes@users.noreply.github.com>
2025-01-03 19:07:06 +1030

    Added map command to query location-area list endpoint

--- repl.go
+++ repl.go
@@ -11,6 +11,7 @@ var commands map[string]cliCommand
 
 func repl() {
 	commands = registerCommands()
+	config := Config{}
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
@@ -22,7 +23,7 @@ func repl() {
 			fmt.Println("Unknown command")
 			continue
 		}
-		err := command.callback()
+		err := command.callback(&config)
 		if err != nil {
 			fmt.Println(err.Error())
 		}
@@ -37,36 +38,80 @@ func cleanInput(text string) []string {
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*Config) error
+}
+
+type Config struct {
+	Next     *string
+	Previous *string
 }
 
 func registerCommands() (commands map[string]cliCommand) {
 	commands = map[string]cliCommand{}
-	commands["exit"] = cliCommand{
-		name:        "exit",
-		description: "Exit the Pokedex",
-		callback:    commandExit,
-	}
 	commands["help"] = cliCommand{
 		name:        "help",
 		description: "Displays list of available commands",
 		callback:    commandHelp,
 	}
+	commands["map"] = cliCommand{
+		name:        "map",
+		description: "Displays the next 20 map locations",
+		callback:    commandMapForward,
+	}
+	commands["mapb"] = cliCommand{
+		name:        "mapb",
+		description: "Displays the previous 20 map locations",
+		callback:    commandMapBack,
+	}
+	commands["exit"] = cliCommand{
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	}
 	return commands
 }
 
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp() error {
+func commandHelp(config *Config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println()
 	for _, command := range commands {
 		fmt.Println(command.name + ": " + command.description)
 	}
+	return nil
+}
+
+func commandMap(url string, config *Config) error {
+	locations, err := getResourceList(url)
+	if err != nil {
+		return err
+	}
+	config.Next = locations.Next
+	config.Previous = locations.Previous
+	for _, location := range locations.Results {
+		fmt.Println(location.Name)
+	}
+	return nil
+}
+
+func commandMapForward(config *Config) error {
+	url := "https://pokeapi.co/api/v2/location-area/"
+	if config.Next != nil {
+		url = *(config.Next)
+	}
+	return commandMap(url, config)
+}
+
+func commandMapBack(config *Config) error {
+	if config.Previous == nil {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+	return commandMap(*config.Previous, config)
+}
+
+func commandExit(config *Config) error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
 	return nil
 }
