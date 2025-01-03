5106dd3c8ef9f06578a3bea03e3ef0eac2dd1df7
Jeremy Hughes <jthughes@users.noreply.github.com>
2025-01-03 16:07:54 +1030

    Basic command map system, with help listing all commands

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := scanner.Text()
-		words := cleanInput(input)
-		fmt.Println("Your command was:", words[0])
-	}
+	repl()
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,72 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var commands map[string]cliCommand
+
+func repl() {
+	commands = registerCommands()
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		words := cleanInput(input)
+		command, ok := commands[words[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		err := command.callback()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	words := strings.Fields(strings.ToLower(text))
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func registerCommands() (commands map[string]cliCommand) {
+	commands = map[string]cliCommand{}
+	commands["exit"] = cliCommand{
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	}
+	commands["help"] = cliCommand{
+		name:        "help",
+		description: "Displays list of available commands",
+		callback:    commandHelp,
+	}
+	return commands
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, command := range commands {
+		fmt.Println(command.name + ": " + command.description)
+	}
+	return nil
 }
